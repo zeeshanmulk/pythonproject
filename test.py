cc78a7d270fe714d692ba7a7b4c4c9560a020829
Zeeshan Mulk <zmlk@mta.ca>
2018-10-13 16:33:08 -0400

    2nd commit

--- test.py
+++ test.py
@@ -1,1 +1,17 @@
-print ("Hello World")
+class Name:
+
+    def __init__(self, firstname, lastname):
+        self.firstname = firstname
+        self.lastname = lastname
+
+    def print_name(self):
+        print (self.firstname + " " + self.lastname)
+
+    def return_firstnamme(self):
+        return self.firstname
+
+
+n1 = Name("Zeeshan", "Mulk")
+n1.print_name()
+
+
